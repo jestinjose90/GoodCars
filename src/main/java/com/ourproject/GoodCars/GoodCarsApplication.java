f80c6e54b2caeaea87eef50219ae055e236cca9a
Jestin Jose <89560571+jestinjose90@users.noreply.github.com>
2022-04-03 08:47:16 -0500

    Add files via upload

---
+++ src/main/java/com/ourproject/GoodCars/GoodCarsApplication.java
@@ -0,0 +1,13 @@
+package com.ourproject.GoodCars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodCarsApplication.class, args);
+	}
+
+}
